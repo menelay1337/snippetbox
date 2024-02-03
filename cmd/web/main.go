43119a65c9ad51c456f72d55f28ab271c82847fa
Murat <muratkairbek908@gmail.com>
2024-02-03 20:23:38 +0600

    integrated connection of MySQL db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"flag"
 	"net/http"
+	"database/sql"
 	"log/slog"
+	"fmt"
+	"flag"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // application structure for dependency injection
@@ -17,18 +21,47 @@ type config struct {
 	port	  string
 	staticDir string
 	logFormat string
+	dsn		  string
 }
 
 var cfg config
 
+// Database functions
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
+	var dsn string;
+	fmt.Print("Enter DSN: ")
+	fmt.Scan(&dsn)
+
 	// assigning flags
 	flag.StringVar(&cfg.port,"port", ":3000", "Port of http server.")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.StringVar(&cfg.logFormat, "format", "text", "format of logs(json or plain text)")
+	flag.StringVar(&cfg.dsn, "DSN", "user:pass@/snippetbox?parseTime=true", "data source name for entering database")
+
+	if dsn != "" {
+		cfg.dsn = dsn
+	}
+
 	// obtaining cmd flags and assigns them to variables'
 	flag.Parse()
-	// application initialize
+
+	// application and logger initialize
 	var logger *slog.Logger
 	var loggerOptions = &slog.HandlerOptions{
 		Level: slog.LevelDebug,
@@ -40,15 +73,25 @@ func main() {
 	} else if cfg.logFormat == "text" {
 		logger = slog.New(slog.NewTextHandler(os.Stdout, loggerOptions))
 	}
-	// application initialize 
+
 	app := &application{
 		logger: logger,
 	}
 
-	// log Start of the server
+	// open db connection pool
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	// start of the server
 	logger.Info("Starting server", slog.Any("port", cfg.port))
 
-	err := http.ListenAndServe(cfg.port, app.routes())
+	err = http.ListenAndServe(cfg.port, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
